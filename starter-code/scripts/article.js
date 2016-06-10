7fe9031a88af6eeda6d50e075778c117b0dea90d
dylanjsa90 <dylanjsanders1@gmail.com>
2016-06-10 09:37:50 -0700

    fixed category filter population

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -9,21 +9,21 @@ function Article (opts) {
   this.publishedOn = opts.publishedOn;
 }
 
-Article.prototype.toHtml = function() {
+Article.prototype.toHtml = function(scriptTemplateId) {
   // TODO: Use handlebars to render your articles!
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-  var source = $('#article-template').html();
-
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced
   //   by a key in the template. For example, you might want to display how old a post is,
   //   or say "(draft)" if it has no publication date:
+  var template = Handlebars.compile($(scriptTemplateId).text());
+
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  var template = Handlebars.compile(source);
   return template(this);
+
 
   // TODO: Use the function that Handlebars gave you to return your filled-in
   //       html template for THIS article.
@@ -49,9 +49,10 @@ ourLocalData.forEach(function(ele) {
 });
 
 articles.forEach(function(a){
-  $('#author-filter').append(a.populateAuthor());
-  $('#articles').append(a.toHtml());
-  if (!$('#category-filter:contains("' + a.category + '")')) {
-    $('#category-filter').append(a.populateCategories());
-  } 
+
+  if ($('#category-filter:contains("' + a.category + '")').length === 0) {
+    $('#category-filter').append(a.toHtml('#category-filter-template'));
+  }
+  $('#author-filter').append(a.toHtml('#author-filter-template'));
+  $('#articles').append(a.toHtml('#article-template'));
 });
